d9a4925213b535202e0bd231a010d36a689fa4e4
LeeSoonGu <rntnsl9280@naver.com>
2024-05-27 20:13:31 +0900

    fetch를 다시 axios 로 변경

--- src/sharing/util/axiosInstance.js
+++ src/sharing/util/axiosInstance.js
@@ -1,28 +1,28 @@
-// import axios from "axios";
+import axios from "axios";
 
-// export const axiosInstance = axios.create({
-//   baseURL: "https://bootcamp-api.codeit.kr/api/",
-// });
+export const axiosInstance = axios.create({
+  baseURL: "https://bootcamp-api.codeit.kr/api/",
+});
 
-const baseURL = "https://bootcamp-api.codeit.kr/api/";
+// const baseURL = "https://bootcamp-api.codeit.kr/api/";
 
-const fetchData = async (endpoint, options = {}) => {
-  try {
-    const response = await fetch(baseURL + endpoint, {
-      ...options,
-      headers: {
-        "Content-Type": "application/json",
-        ...options.headers,
-      },
-    });
+// const fetchData = async (endpoint, options = {}) => {
+//   try {
+//     const response = await fetch(baseURL + endpoint, {
+//       ...options,
+//       headers: {
+//         "Content-Type": "application/json",
+//         ...options.headers,
+//       },
+//     });
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! status: ${response.status}`);
+//     }
 
-    return await response.json();
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    throw error;
-  }
-};
+//     return await response.json();
+//   } catch (error) {
+//     console.error("Error fetching data:", error);
+//     throw error;
+//   }
+// };
