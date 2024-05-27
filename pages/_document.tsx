43934bd27a1dd561a8b6825874d9381acc45e387
LeeSoonGu <rntnsl9280@naver.com>
2024-05-27 19:54:37 +0900

    14주차 템플릿 코드 옮기기

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import { Html, Head, Main, NextScript } from "next/document";
-=======
 import { Html, Head, Main, NextScript } from 'next/document'
->>>>>>> 0c228dc3519b93046522150c1ceaf9b57e042d91
 
 export default function Document() {
   return (
@@ -13,9 +9,5 @@ export default function Document() {
         <NextScript />
       </body>
     </Html>
-<<<<<<< HEAD
-  );
-=======
   )
->>>>>>> 0c228dc3519b93046522150c1ceaf9b57e042d91
 }
